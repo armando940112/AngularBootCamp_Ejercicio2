651e0fc1767f92f6f5cf9a532a595a5c86f6bf16
ALondono <armando.londono@yuxipacific.com>
2018-02-07 13:25:24 -0500

    Generating random images every second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   author = 'Armando Londoño';
+  imageUrl: string;
+  baseUrl = 'https://api.adorable.io/avatars/500/';
+  urlSuffix = '@adorable.io.png';
+
+  constructor() {
+    setInterval( () =>
+      this.generateRandomFace(), 1000
+    );
+  }
+  
+  generateRandomFace() {
+    this.imageUrl = `${this.baseUrl}${this.getRandomImageId()}${this.urlSuffix}`;
+  }
+
+  getRandomImageId(): string {
+    return Math.random().toString();
+  }
 }
